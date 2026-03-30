8006cb8bc88b09a803907a8e274ded252cf65452
madsaomi13 <madsaomi13@gmail.com>
2026-03-30 09:21:58 -0700

    refactor: move app initialization to module level in app.py

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ def create_app():
 
 import os
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
